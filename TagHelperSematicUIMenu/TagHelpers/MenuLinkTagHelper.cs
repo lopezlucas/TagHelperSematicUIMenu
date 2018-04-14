3de354d58034d4b6a3237647f7f66e082d376a5a
Lopez Oderda, Lucas F <Lucas.F.Lopez.Oderda@kcc.com>
2018-04-13 23:47:06 -0300

    Add project files.

---
+++ TagHelperSematicUIMenu/TagHelpers/MenuLinkTagHelper.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TagHelperSematicUIMenu.TagHelpers
+{
+    public class MenuLinkTagHelper : TagHelper
+    {
+
+        [ViewContext]
+        public ViewContext ViewContext { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "a";    // Replaces <menu-link> with <a> tag
+
+            string page = ViewContext.RouteData.Values["page"].ToString();
+            string href = context.AllAttributes["href"].Value.ToString();
+
+            if (page.Equals(href, StringComparison.OrdinalIgnoreCase))
+            {
+                var classes = output.Attributes.FirstOrDefault(a => a.Name == "class")?.Value;
+                output.Attributes.SetAttribute("class", $"{classes} active ");
+            }
+
+        }
+    }
+}
